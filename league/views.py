060aec1edbdc85544acd167db1bfc1c7c10ffc6b
James King <jamesking.1711@gmail.com>
2024-09-17 10:11:52 +0000

    Added template inheritance and base pages with dummy data.

--- league/views.py
+++ league/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render, HttpResponse
 
+players = [
+    {
+        'name': 'James King',
+        'faction': 'Tyranids',
+        'league_points': '237',
+        'games_played': '2',
+        'league_position': '4'
+    },
+    {
+        'name': 'Jimmy King',
+        'faction': 'Space Marines',
+        'league_points': '289',
+        'games_played': '4',
+        'league_position': '2'
+    },
+    {
+        'name': 'Peter Scott',
+        'faction': 'Grey Knights',
+        'league_points': '301',
+        'games_played': '4',
+        'league_position': '1'
+    },
+    {
+        'name': 'Jack White',
+        'faction': 'Tyranids',
+        'league_points': '299',
+        'games_played': '3',
+        'league_position': '2'
+    },
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Welcome to the Newbury 40K League</h1>")
+    context = {
+        'players': players
+    }
+    return render(request, "league/home.html", context)
 
 def about(request):
-    return HttpResponse("<h1>This is a local warhammer 40K league based out of Newbury, Berkshire</h1>")
+    return render(request, "league/about.html", {'title': 'About the Newbury 40K League'})
