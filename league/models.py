0c37ce779d0301dcad0ac93413ff042989b7f78c
James King <jamesking.1711@gmail.com>
2024-10-02 11:16:33 +0000

    Updated player results submission.

--- league/models.py
+++ league/models.py
@@ -11,27 +11,12 @@ class Player(models.Model):
     games_played = models.PositiveIntegerField(default=0)
     league_points = models.PositiveIntegerField(default=0)
     victory_points_tally = models.PositiveIntegerField(default=0)
+    player_vp = models.PositiveIntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse("player-detail", kwargs={"pk": self.pk})
 
-    def update_stats(self, player_score):
-        self.games_played += 1
-        self.victory_points_tally += player_score
-        self.save
-
     def __str__(self):
         return self.name
 
-    
-class Game(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    player_vp = models.PositiveIntegerField(default=0)
-
-    def submit_score(self, player_score):
-        self.player.update_stats(self.player_vp)
-        super().save()
-
-    def __str__(self):
-        return f"Results submitted successfully"
     
--- league/views.py
+++ league/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 from django.urls import reverse_lazy
 
 from . import models
@@ -53,14 +54,27 @@ class PlayerDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 class SubmitResultsView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = models.Player
     template_name = 'league/submitresults.html'
-    fields = ['name', 'faction']
+    fields = ['victory_points_tally', 'games_played']
 
     def test_func(self):
         player = self.get_object()
         return self.request.user == player.user
+    
+    def update_stats(request):
+        if request.method == 'POST':
+            results = request.POST['results']
+            score = self(results=results)
+            self.games_played += 1
+            self.victory_points_tally += score
+            self.save
+            messages.success("Results submitted successfully")
+            return redirect("league/home.html")
+        else:
+            return render(request, "league/home.html")
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        update_stats()
         return super().form_valid(form)
 
     
