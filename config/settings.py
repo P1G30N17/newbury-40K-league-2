9cb5eb92002bb550abad1b2c066073c63e1cf5a3
James King <jamesking.1711@gmail.com>
2024-10-07 09:32:15 +0000

    Set up files for heroku deployment.

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+@_lx@a1)j$oh+kj%givt117z6m(dd+&ram$=e4x$p_*%qm7v2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-p1g30n17-newbury40kleag-0caiuqmos63.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-p1g30n17-newbury40kleag-0caiuqmos63.ws.codeinstitute-ide.net', '.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://8000-p1g30n17-newbury40kleag-0caiuqmos63.ws.codeinstitute-ide.net']
 CSRF_ALLOWED_ORIGINS = ['https://8000-p1g30n17-newbury40kleag-0caiuqmos63.ws.codeinstitute-ide.net']
 CORS_ORIGINS_WHITELIST = ['https://8000-p1g30n17-newbury40kleag-0caiuqmos63.ws.codeinstitute-ide.net']
